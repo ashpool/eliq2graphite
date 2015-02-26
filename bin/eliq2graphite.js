b717d01ae1c59fe39838a10171a026ce2c9ecff9
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-26 20:39:06 +0100

    Extract eliq stuff to eliq-promise project https://github.com/ashpool/eliq-promise

--- bin/eliq2graphite.js
+++ bin/eliq2graphite.js
@@ -1,6 +1,6 @@
 var home = require('./../lib/homepath'),
 	config = require(home.path() + '/.eliq2graphite/config.json'),
-	eliq = require('./../lib/eliq')(config),
+	eliq = require('eliq-promise')(config),
 	graphite = require('./../lib/graphite')(config);
 
 eliq.getToday().then(graphite.log);
--- test/index.js
+++ test/index.js
@@ -1,5 +1,3 @@
-require('./eliqurl');
-require('./date');
 require('./homepath');
 require('./metric');
 
