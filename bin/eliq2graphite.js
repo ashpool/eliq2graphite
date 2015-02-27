4e04b62956d8f45b22332c220b3bef266a37cb2e
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-02-27 21:12:36 +0100

    Up dep eliq-promise to 0.0.4

--- bin/eliq2graphite.js
+++ bin/eliq2graphite.js
@@ -1,6 +1,1 @@
-var home = require('./../lib/homepath'),
-	config = require(home.path() + '/.eliq2graphite/config.json'),
-	eliq = require('eliq-promise')(config),
-	graphite = require('./../lib/graphite')(config);
-
-eliq.getToday().then(graphite.log);
+require('./day');
---
+++ bin/hour.js
@@ -0,0 +1,6 @@
+var home = require('./../lib/homepath'),
+    config = require(home.path() + '/.eliq2graphite/config.json'),
+    eliq = require('eliq-promise')(config),
+    graphite = require('./../lib/graphite')(config);
+
+eliq.getThisHour().then(graphite.log);
