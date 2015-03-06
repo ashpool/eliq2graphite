91c43013e1a103dc34a9711f03b017cc55e232b2
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-06 21:34:36 +0100

    Log energy

--- test/metric.js
+++ test/metric.js
@@ -1,26 +1,39 @@
 /*jshint undef:false */
 var chai = require('chai'),
-	expect = chai.expect;
+    expect = chai.expect;
 
 describe('metric', function () {
-	describe('#create', function () {
-		it('creates an object based on format string', function () {
-			var format = 'home.power.average',
-				value = 1337,
-				metric = require('../lib/metric');
-			expect(metric.create(format, value).home.power.average).to.equal(1337);
-		});
-		it('format string can be anything', function () {
-			var format = 'fuu.bar.boo.baz.fuz.foz',
-				value = 43,
-				metric = require('../lib/metric');
-			expect(metric.create(format, value).fuu.bar.boo.baz.fuz.foz).to.equal(43);
-		});
-		it('format string can be just one word', function () {
-			var format = 'averagepower',
-				value = 1234,
-				metric = require('../lib/metric');
-			expect(metric.create(format, value).averagepower).to.equal(1234);
-		});
-	});
+    describe('#create', function () {
+        it('creates an object based on format string', function () {
+            var format = 'eliq',
+                value = {
+                    avgpower: 1337,
+                    energy: 1234
+                },
+                metric = require('../lib/metric');
+            expect(metric.create(format, value).eliq.avgpower).to.equal(1337);
+            expect(metric.create(format, value).eliq.energy).to.equal(1234);
+        });
+        it('format string can be anything', function () {
+            var format = 'fuu.bar.boo.baz.fuz.foz',
+                value = {
+                    avgpower: 43,
+                    energy: 44
+                },
+                metric = require('../lib/metric');
+            expect(metric.create(format, value).fuu.bar.boo.baz.fuz.foz.avgpower).to.equal(43);
+            expect(metric.create(format, value).fuu.bar.boo.baz.fuz.foz.energy).to.equal(44);
+        });
+        it('format string can be just one word', function () {
+            var format = 'averagepower',
+                value = {
+                    avgpower: 1234,
+                    energy: 4321
+                },
+                metric = require('../lib/metric');
+            console.log(metric.create(format, value));
+            expect(metric.create(format, value).averagepower.avgpower).to.equal(1234);
+            expect(metric.create(format, value).averagepower.energy).to.equal(4321);
+        });
+    });
 });
