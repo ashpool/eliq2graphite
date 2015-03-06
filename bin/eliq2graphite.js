1b95594e81b98ffc86fccc4a675b8800ed74c84f
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-06 14:33:26 +0100

    Add 'day' as resolution. Rename variable

--- bin/eliq2graphite.js
+++ bin/eliq2graphite.js
@@ -10,7 +10,7 @@ eliq2graphite [--age number-of-hours] [--resolution hour|6min]\n\
 \n\
 Options: \n\
 -a, --age=number of hours (default 2 hours)\n\
--r, --resolution=hour|6min (default 6min) \n\
+-r, --resolution=6min|hour|day (default 6min) \n\
 -h, --help (show this)\
 \n';
 
