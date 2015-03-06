fc90372cfa3c3a22da29f58ae9cd76d47767da0a
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-03-06 14:31:43 +0100

    Up dep eliq-promise to 0.1.0

--- bin/day.js
+++
@@ -1,6 +0,0 @@
-var home = require('./../lib/homepath'),
-    config = require(home.path() + '/.eliq2graphite/config.json'),
-    eliq = require('eliq-promise')(config),
-    graphite = require('./../lib/graphite')(config);
-
-eliq.getToday().then(graphite.log).catch(console.log);
