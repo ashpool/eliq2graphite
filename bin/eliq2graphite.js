478192374bba9f0610536526c7cc6a2b1bcde8f7
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-04-07 17:50:48 +0200

    Log error

--- bin/eliq2graphite.js
+++ bin/eliq2graphite.js
@@ -2,6 +2,7 @@ var argv = require('minimist')(process.argv.slice(2)),
     home = require('./../lib/homepath'),
     config = require(home.path() + '/.eliq2graphite/config.json'),
     eliq = require('eliq-promise')(config),
+    logger = require('log4js-extras')(config).getLogger(__filename),
     graphite = require('./../lib/graphite')(config);
 
 var usage = '\
@@ -17,5 +18,9 @@ Options: \n\
 if (argv.h || argv.help) {
     console.log(usage);
 } else {
-    eliq.getFrom(24 || argv.a || argv.age || 2, 'hour' || argv.r || argv.resolution || '6min').then(graphite.log);
+    eliq.getFrom(24 || argv.a || argv.age || 2, 'hour' || argv.r || argv.resolution || '6min').then(function (value) {
+        graphite.log(value)
+    }, function (value) {
+        logger.error(value);
+    });
 }
