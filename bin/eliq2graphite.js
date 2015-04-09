652a22791fc83eaf1d490025885ca5ebf49eb49c
Magnus Ljadas <magnus.ljadas@gmail.com>
2015-04-09 19:43:50 +0200

    Fix hardcoded resolution

--- bin/eliq2graphite.js
+++ bin/eliq2graphite.js
@@ -18,5 +18,5 @@ Options: \n\
 if (argv.h || argv.help) {
     console.log(usage);
 } else {
-    eliq.getFrom(24 || argv.a || argv.age || 2, 'hour' || argv.r || argv.resolution || '6min').then(graphite.log).catch(logger.error);
+    eliq.getFrom(24 || argv.a || argv.age || 2,  argv.r || argv.resolution || '6min').then(graphite.log).catch(logger.error);
 }
