0c3ac397fee60c2317fb0ef145900ef8f889006c
Magnus Ljadas <magnus.ljadas@gmail.com>
2018-02-24 20:36:40 +0100

    Add tests for lib/graphite

---
+++ test/graphite.js
@@ -0,0 +1,56 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+
+describe('graphite', function () {
+    describe('#logSnapshot', function () {
+        it('logs a single snapshot power measurement', function () {
+            const snapshot = {
+                createddate: '2018-02-23T16:15:44',
+                power: 2661
+            };
+            const stub = sinon.stub();
+            const client = { write: stub.resolves({}) };
+            const config = { client: client };
+            const graphite = require('../lib/graphite')(config);
+            graphite.logSnapshot(snapshot);
+
+            expect(client.write.calledOnce).to.be.true;
+            expect(client.write.calledWith({ eliq: { snapshot: { power: 2661 } } }, 1519398944000)).to.be.true;
+        });
+    });
+
+    describe('#log', function () {
+        it('logs a bunch of measurements', function () {
+            const period = {
+                startdate: '2015-02-12T00:00:00',
+                enddate: '2015-02-13T00:00:00',
+                intervaltype: 'hour',
+                data:
+                    [{
+                        avgpower: 1710,
+                        energy: 1709,
+                        temp_out: 1,
+                        time_start: '2015-02-12T00:00:00',
+                        time_end: '2015-02-12T01:00:00'
+                    },
+                    {
+                        avgpower: 1820,
+                        energy: 1821,
+                        temp_out: 2,
+                        time_start: '2015-02-12T16:00:00',
+                        time_end: '2015-02-12T17:00:00'
+                    }]
+            };
+            const stub = sinon.stub();
+            const client = { write: stub.resolves({}) };
+            const config = { client: client };
+            const graphite = require('../lib/graphite')(config);
+            graphite.log(period);
+
+            expect(client.write.calledTwice).to.be.true;
+            expect(client.write.calledWith({ eliq: { hour: { avgpower: 1710, energy: 1709 } } }, 1423697400000)).to.be.true;
+            expect(client.write.calledWith({ eliq: { hour: { avgpower: 1820, energy: 1821 } } }, 1423755000000)).to.be.true;
+        });
+    });
+})
